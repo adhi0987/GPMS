f39dd45ac084c8d2a5964165e114e94aca0865c5
adhi0987 <adhithyashivakumar2511@gmail.com>
2025-07-05 14:38:02 +0530

    added some gaps in urls.py for clarity

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -29,30 +29,38 @@ urlpatterns = [
 
      path('citizens/addcomplaints',views.addcomplaints,name='addcomplaints'),
 
+     path('panchayat_employees/',views.panemp,name='panchayat_employees'),
+     path('panchayat_employees/addcitizen/',views.addcitizen,name="addcitizen"),
+     path('panchayat_employees/updateCitizen/',views.updateCitizen,name="updateCitizen"),
+
+     path('panchayat_employees/addland/',views.addland,name="addland"),
+     path('panchayat_employees/updateLand/',views.updateLand,name="updateLand"),
+     path('panchayat_employees/previousOwners/',views.previousOwners,name="previousOwners"),
+
+     path('panchayat_employees/issuecertificate/',views.issuecertificate,name="issuecertificate"),
+
+     path('panchayat_employees/paymentPage',views.citizenPayments,name="panchayat_taxpay"),
+     path('panchayat_employees/enroll_eligible_members/', views.enroll_eligible_members, name='enroll_eligible_members'),
+     path('panchayat_employees/enrolltoschemes/',views.enrolltoschemes,name="enrolltoschemes"),
+     
+     path('panchayat_employees/viewscheme/',views.viewscheme,name="viewscheme"),
+     path("panchayat_employees/delete_scheme/<int:scheme_id>/", views.delete_scheme, name="delete_scheme"),
+     path('panchayat_employees/addschemes/',views.addschemes,name="addschemes"),
+     
+     path('panchayat_employees/edit_asset/', views.edit_asset, name="edit_asset"),
+     path("panchayat_employees/delete_asset/<int:asset_id>/", views.delete_asset, name="delete_asset"),
+     path('panchayat_employees/addassets/',views.addassets,name="addassets"),
+     
+     path('panchayat_employees/addhousehold/',views.addhousehold,name="addhousehold"),
+     
+     path('panchayat_employees/addNotification/',views.addNotification,name="addNotification"),
 
      path('govt_monitors',views.govt_monitors,name='govt_monitors'),
-     path('panchayat_employees',views.panemp,name='panchayat_employees'),
-     path('addcitizen',views.addcitizen,name="addcitizen"),
-     path('addland',views.addland,name="addland"),
-     path('issuecertificate',views.issuecertificate,name="issuecertificate"),
-     path('enrolltoschemes',views.enrolltoschemes,name="enrolltoschemes"),
-     path('addschemes',views.addschemes,name="addschemes"),
      path('Admin', views.Admin, name = 'Admin'),
      path('Admin/addGovtMonitor_admin', views.addGovtMonitor_admin, name = 'addGovtMonitor_admin'),
      path('Admin/addemployee_admin', views.addemployee_admin, name = 'addemployee_admin'),  
      path('Admin/deleteGM/',views.inactiveGM,name = 'inactiveGM'),
      path('Admin/deletePE/',views.inactivePE,name = 'inactivePE'),
-     path('addassets',views.addassets,name="addassets"),
-     path('addhousehold',views.addhousehold,name="addhousehold"),
-     path('updateCitizen/',views.updateCitizen,name="updateCitizen"),
-     path('updateLand/',views.updateLand,name="updateLand"),
-     path('viewscheme/',views.viewscheme,name="viewscheme"),
-     path("delete_scheme/<int:scheme_id>/", views.delete_scheme, name="delete_scheme"),
-     path('previousOwners/',views.previousOwners,name="previousOwners"),
-     path('edit_asset/', views.edit_asset, name="edit_asset"),
-     path("delete_asset/<int:asset_id>/", views.delete_asset, name="delete_asset"),
      path('update_all_taxes/', views.update_all_taxes, name='update_all_taxes'),
-     path('enroll_eligible_members/', views.enroll_eligible_members, name='enroll_eligible_members'),
-     path('addNotification',views.addNotification,name="addNotification")
 
 ]
